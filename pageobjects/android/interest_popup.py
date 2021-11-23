5823a17d64a0da57a6ab96b0594725ad084f7442
sunping <sunping@kuaishou.com>
2021-11-24 00:02:12 +0800

    add interest_tag

---
+++ pageobjects/android/interest_popup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import os
+
+from krunner.core.android.element import AndroidElement
+from krunner.core.base import ImgElement
+
+class InterestPopup(object):
+    """兴趣标签弹窗"""
+
+    curr_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'template')
+
+    # tab_recrod = ImgElement(template=curr_dir + '/record.png', annotation='录制按钮')
+    # agree_button = AndroidElement(resourceId='com.smile.gifmaker:id/positive', description='同意并继续', annotation='同意按钮')
+    #agree_button = AndroidElement(text='同意并继续', annotation='同意按钮')
+    # disagree_button = AndroidElement(text='不同意', annotation='不同意按钮')
+    # disagree_again_button = AndroidElement(text='仍不同意', annotation='P2 页的仍不同意按钮')
+    # check_policy_button = AndroidElement(text='查看协议', annotation='查看协议按钮')
+    # enter_visitor_model_button = AndroidElement(text='进入访客模式', annotation='进入访客模式按钮')
+    # exit_button = AndroidElement(text='退出应用', annotation='进入访客模式按钮')
+    """通过元素定位进入设置页"""
+
+    # return_back = AndroidElement(description='返回', index=0, annotation='返回按钮')
+    # menu_btn=AndroidElement(resourceId='com.smile.gifmaker:id/left_btn',annotation='菜单按钮')
+    # set_btn=AndroidElement(resourceId='com.smile.gifmaker:id/setting_iv',annotation='设置按钮')
+    # about_btn=AndroidElement(text='关于我们',annotation='关于我们按钮')
+    #logo_btn=AndroidElement(resourceId='com.smile.gifmaker:id/logo',annotation='快手图标')
+
+    """快速进入设置页"""
+    logo_btn=AndroidElement(resourceId='com.smile.gifmaker:id/logo',annotation='快手图标')
+
+    #btn_channel = AndroidElement(resourceId='id/version_tv',  annotation='渠道号')
+    btn_channel2 = AndroidElement(resourceId='id/channel', annotation='预装渠道号')
+    growth_btn=AndroidElement(text='增长',annotation='摇一摇增长tab')
+
+
+
+
+
