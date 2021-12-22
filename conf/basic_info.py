79d72059c066946beb279e5765b90b8a83341547
yuside <yuside@kuaishou.com>
2021-12-22 20:49:44 +0800

    有些机型不支持 adb shell pm clear 清数据，卸载重装解决

--- conf/basic_info.py
+++ conf/basic_info.py
@@ -3,6 +3,7 @@ from conf.schemes import Scheme
 
 sn = get_config_value('serialno')[0]
 pkg = get_config_value('pkg_name')
+apk_path = get_config_value('pkg_path')
 re_apk_url = get_config_value('re_apk_url')
 
 scheme = Scheme(pkg)
