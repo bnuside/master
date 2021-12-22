7895c813c231a7d1f0e22a3edf8a5b71b5d0cf60
yuside <yuside@kuaishou.com>
2021-12-22 16:29:11 +0800

    渠道号覆盖安装用例

--- conf/channels.py
+++ conf/channels.py
@@ -1,14 +1,18 @@
 import time
+from krunner.utils import adb
 
 
 def get_channel(brand, year=2021):
+    brand = brand.strip().upper()
+    if 'ONEPLUS' in brand:
+        brand = 'OPPO'
     if year <= 2021:
-        return f'KWAI_{brand.upper()}'
+        return f'{brand}_KWAI'
     if isinstance(year, int):
-        return f'{brand.upper()}_YZ_{year}'
+        return f'{brand}_YZ_{year}'
     else:
-        return f'{brand.upper()}_YZ_{time.localtime(time.time()).tm_year}'
+        return f'{brand}_YZ_{time.localtime(time.time()).tm_year}'
 
 
 if __name__ == '__main__':
-    print(get_channel('HONOR', 2025.0))
+    print(get_channel(adb.get_device_brand('da0bbd02'), 2025.0))
--- testcase/android/test_oem_chanel_protection.py
+++ testcase/android/test_oem_chanel_protection.py
@@ -2,6 +2,7 @@ import os
 import time
 from krunner.utils import logger
 from krunner.utils import adb
+from krunner.utils import get_local_package_path
 from krunner.conf import get_config_value
 from krunner.plugins.login import LoginTool
 
@@ -27,6 +28,40 @@ class TestOEMChannelProtection(KRunner):
 
     def test_new_install(self):
         brand = adb.get_device_brand(sn)
+        logger.debug(f'brand={brand}')
+        self.driver.open_schema(scheme.settings)
+        time.sleep(5)
+        retry_count = 5
+        while retry_count > 0 and not Settings.about_us.exist(1):
+            self.driver.swipe_up()
+            time.sleep(1)
+            retry_count -= 1
+        Settings.about_us.click()
+        time.sleep(5)
+        version_view = AboutUs.version
+        assert version_view.exist(2)
+        x = (version_view.info['bounds']['left'] + version_view.info['bounds']['right']) // 2
+        y = (version_view.info['bounds']['top'] + version_view.info['bounds']['bottom']) // 2
+        cmd = f"adb -s {sn} shell input tap {x} {y}"
+        for i in range(8):
+            os.popen(cmd)
+            time.sleep(0.1)
+        time.sleep(2)
+        target = AboutUs.channel.find_element().get_text()
+        logger.debug(f'target={target}')
+        logger.debug(f'exp={get_channel(brand)}')
+        self.assert_equal(f'预期渠道号是：[{get_channel(brand)}]，实际渠道号是：[{target}]', target, get_channel(brand))
+
+    def test_re_install_instant(self):
+        adb.clear_app_data(sn, pkg)
+        time.sleep(5)
+        re_apk_path = get_local_package_path(re_apk_url)
+        adb.install_app(sn, re_apk_path)
+        time.sleep(60)
+
+        brand = adb.get_device_brand(sn)
+        logger.debug(brand)
+        logger.debug(get_channel(brand))
         self.driver.open_schema(scheme.settings)
         time.sleep(5)
         retry_count = 5
